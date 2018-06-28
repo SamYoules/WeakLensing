72f98ff261fe093034393aa5f5e25a55a3d68898
Samantha Youles <youless@node236.prv.sciama.cluster>
2018-06-28 15:55:59 +0100

    Added gamma estimator to do_kappa.py

---
+++ bin/do_kappa.py
@@ -0,0 +1,386 @@
+#!/usr/bin/env python
+
+import numpy as N
+import pylab as P
+import scipy as sp
+import fitsio
+from astropy.io import fits
+import argparse
+import glob
+import healpy
+import sys
+from scipy import random
+import copy
+
+from picca import constants
+from picca.data import delta
+
+from multiprocessing import Pool,Process,Lock,Manager,cpu_count,Value
+
+import configargparse
+
+
+
+class kappa:
+
+    nside = 64
+    nside_data = 32
+    rot = healpy.Rotator(coord=['C', 'G'])
+    lambda_abs = 1215.67
+
+    fid_Om=0.31
+    cosmo = constants.cosmo(fid_Om)
+
+    rt_min=0.
+    rt_max=40.
+    rp_min=0.
+    rp_max=10.
+    nt = 1
+    np = 1
+
+    xi2d = None
+
+    angmax = None
+    z_min_pix = None
+
+    counter = Value('i',0)
+    lock = Lock()
+
+    data={}
+    ndata=0
+
+    @staticmethod
+    def load_model(modelfile, nbins=50) :
+
+        data_rp, data_rt, xi_dist = N.loadtxt(modelfile, unpack=1)
+
+        #-- get the larger value of the first separation bin to make a grid
+        rp_min = data_rp.reshape(50, 50)[0].max()
+        rp_max = data_rp.reshape(50, 50)[-1].min()
+        rt_min = data_rt.reshape(50, 50)[:, 0].max()
+        rt_max = data_rt.reshape(50, 50)[:, -1].min()
+        #-- create the regular grid for griddata
+        rp = N.linspace(rp_min, rp_max, nbins)
+        rt = N.linspace(rt_min, rt_max, nbins)
+        xim = sp.interpolate.griddata((data_rt, data_rp), xi_dist, \
+                    (rt[:, None], rp[None, :]), method='cubic')
+
+        #-- create interpolator object
+        xi2d = sp.interpolate.RectBivariateSpline(rt, rp, xim)
+
+        kappa.xi2d = xi2d
+        return xi2d
+
+    @staticmethod
+    def read_deltas(in_dir, nspec=None):
+        data = {}
+        ndata = 0
+        dels = []
+
+        fi = glob.glob(in_dir+"/*.fits.gz")
+        for i,f in enumerate(fi):
+            sys.stderr.write("\rread {} of {} {}".format(i,len(fi),ndata))
+            hdus = fitsio.FITS(f)
+            dels += [delta.from_fitsio(h) for h in hdus[1:]]
+            ndata+=len(hdus[1:])
+            hdus.close()
+            if nspec and ndata > nspec:
+                break
+
+        phi = [d.ra for d in dels]
+        th = [sp.pi/2.-d.dec for d in dels]
+        pix = healpy.ang2pix(kappa.nside_data, th, phi)
+
+        z_min_pix = 10**dels[0].ll[0]/kappa.lambda_abs-1
+
+        for d,p in zip(dels,pix):
+            if p not in data:
+                data[p]=[]
+            data[p].append(d)
+
+            z = 10**d.ll/kappa.lambda_abs-1.
+            z_min_pix = sp.amin( sp.append([z_min_pix], z) ) 
+            d.z = z
+            d.r_comov = kappa.cosmo.r_comoving(z)
+            d.we *= ((1.+z)/(1.+2.25))**(2.9-1.)
+            d.project()
+
+        kappa.z_min_pix = z_min_pix
+        kappa.angmax = 2.*\
+                sp.arcsin(kappa.rt_max/(2.*kappa.cosmo.r_comoving(z_min_pix)))
+        kappa.ndata = ndata
+        kappa.data = data
+
+        return data
+
+    @staticmethod
+    def fill_neighs():
+        data = kappa.data
+        print('\n Filling neighbors')
+        for ipix in data.keys():
+            for d1 in data[ipix]:
+                npix = healpy.query_disc(kappa.nside_data, \
+                        [d1.xcart, d1.ycart, d1.zcart], \
+                        kappa.angmax, inclusive = True)
+                npix = [p for p in npix if p in data]
+                neighs = [d for p in npix for d in data[p]]
+                ang = d1^neighs
+                w = ang<kappa.angmax
+                neighs = sp.array(neighs)[w]
+                d1.neighs = [d for d in neighs if d1.ra > d.ra]
+        
+    @staticmethod
+    def get_kappa(pixels):
+
+        ikappa = []
+        skappa = {} 
+        wkappa = {}
+        gamma1 = {} # SY
+        gamma2 = {} # SY
+
+        for ipix in pixels:
+            for i,d1 in enumerate(kappa.data[ipix]):
+                sys.stderr.write("\rcomputing kappa: {}%".format(\
+                                 round(kappa.counter.value*100./kappa.ndata,2)))
+                with kappa.lock:
+                    kappa.counter.value += 1
+                for d2 in d1.neighs:
+                    #--  compute the cartesian mid points and convert back 
+                    #--  to ra, dec
+                    mid_xcart = 0.5*(d1.xcart+d2.xcart)
+                    mid_ycart = 0.5*(d1.ycart+d2.ycart)
+                    mid_zcart = 0.5*(d1.zcart+d2.zcart)
+                    mid_ra, mid_dec = get_radec(\
+                        N.array([mid_xcart, mid_ycart, mid_zcart]))
+
+                    #-- apply rotation into Galactic coordinates
+                    #th, phi = kappa.rot(sp.pi/2-mid_dec, mid_ra)
+                    #-- keeping without rotation
+                    th, phi = sp.pi/2-mid_dec, mid_ra
+                    th1, phi1 = sp.pi/2-d1.dec, d1.ra # SY
+                    th2, phi2 = sp.pi/2-d2.dec, d2.ra # SY
+
+                    #-- check if pair of skewers belong to same spectro
+                    same_half_plate = (d1.plate == d2.plate) and\
+                            ( (d1.fid<=500 and d2.fid<=500) or \
+                            (d1.fid>500 and d2.fid>500) )
+
+                    #-- angle between skewers
+                    ang = d1^d2
+                    if kappa.true_corr:
+                        ang_delensed = d1.delensed_angle(d2)
+
+                    #-- getting pixel in between 
+                    mid_pix = healpy.ang2pix(kappa.nside, \
+                                  th, phi) 
+
+                    if kappa.true_corr:
+                        sk, wk, g1, g2 = kappa.fast_kappa_true(\
+                                d1.z, d1.r_comov, \
+                                d2.z, d2.r_comov, \
+                                ang_delensed, ang, \
+                                th1, phi1, th2, phi2) # SY
+                    else:
+                        sk, wk, g1, g2 = kappa.fast_kappa(\
+                                d1.z, d1.r_comov, d1.we, d1.de, \
+                                d2.z, d2.r_comov, d2.we, d2.de, \
+                                ang, same_half_plate, \
+                                th1, phi1, th2, phi2) # SY
+
+                    if mid_pix in ikappa:
+                        skappa[mid_pix]+=sk
+                        wkappa[mid_pix]+=wk
+                        gamma1[mid_pix]+=g1 # SY
+                        gamma2[mid_pix]+=g2 # SY
+                    else:
+                        ikappa.append(mid_pix)
+                        skappa[mid_pix]=sk
+                        wkappa[mid_pix]=wk
+                        gamma1[mid_pix]=g1 # SY
+                        gamma2[mid_pix]=g2 # SY
+
+                setattr(d1, "neighs", None)
+                
+        return ikappa, skappa, wkappa, gamma1, gamma2 # SY
+
+    @staticmethod
+    def fast_kappa(z1,r1,w1,d1,z2,r2,w2,d2,ang,same_half_plate, \
+                   th1, phi1, th2, phi2): # SY
+        rp = abs(r1-r2[:,None])*sp.cos(ang/2)
+        rt = (r1+r2[:,None])*sp.sin(ang/2)
+        d12 = d1*d2[:, None]
+        w12 = w1*w2[:,None]
+
+        w = (rp>=kappa.rp_min) & (rp<=kappa.rp_max) & \
+            (rt<=kappa.rt_max) & (rt>=kappa.rp_min)
+
+        rp = rp[w]
+        rt = rt[w]
+        w12 = w12[w]
+        d12 = d12[w]
+
+        x = (phi2 - phi1)/sp.sin(th1) # SY
+        y = th2 - th1                 # SY
+        gamma_ang = sp.arctan2(y,x)   # SY
+
+        #-- getting model and first derivative
+        xi_model = kappa.xi2d(rt, rp, grid=False)
+        xip_model = kappa.xi2d(rt, rp, dx=1, grid=False)
+        R = -1/(xip_model*rt)
+       
+        ska = sp.sum( (d12 - xi_model)/R*w12 )
+        wka = sp.sum( w12/R**2 ) 
+        gam1 = 2*sp.cos(2*gamma_ang) * sp.sum( (d12 - xi_model)/R*w12 ) # SY
+        gam2 = -2*sp.sin(2*gamma_ang) * sp.sum( (d12 - xi_model)/R*w12 ) # SY
+
+        return ska, wka, gam1, gam2 # SY
+
+    @staticmethod
+    def fast_kappa_true(z1, r1, z2, r2, ang, ang_lens, \
+                        th1, phi1, th2, phi2): # SY
+        
+        rp      = abs(r1-r2[:,None])*sp.cos(ang/2)
+        rt      = (r1+r2[:,None])*sp.sin(ang/2)
+        rp_lens = abs(r1-r2[:,None])*sp.cos(ang_lens/2)
+        rt_lens = (r1+r2[:,None])*sp.sin(ang_lens/2)
+        
+        #z = (z1+z2[:,None])/2
+
+        w = (rp>=kappa.rp_min) & (rp<=kappa.rp_max) & \
+            (rt<=kappa.rt_max) & (rt>=kappa.rp_min)
+
+        rp = rp[w]
+        rt = rt[w]
+        rp_lens = rp_lens[w]
+        rt_lens = rt_lens[w]
+        #z  = z[w]
+
+        x = (phi2 - phi1)/sp.sin(th1) # SY
+        y = th2 - th1                 # SY
+        gamma_ang = sp.arctan2(y,x)   # SY
+
+        #-- getting model and first derivative
+        xi_model  = kappa.xi2d(rt,      rp,       grid=False)
+        xi_lens   = kappa.xi2d(rt_lens, rp_lens,  grid=False)
+        xip_model = kappa.xi2d(rt,      rp, dx=1, grid=False)
+        R = -1/(xip_model*rt)
+
+        #ska = sp.sum( (xi_lens - xi_model)*R )
+        #wka = xi_lens.size
+        ska = sp.sum( (xi_lens - xi_model)/R )
+        wka = sp.sum( 1/R**2  )
+        gam1 = 2*sp.cos(2*gamma_ang) * sp.sum( (xi_lens - xi_model)/R )  # SY
+        # SY added minus sign below, as g2 is imaginary part of complex shear
+        gam2 = -2*sp.sin(2*gamma_ang) * sp.sum( (xi_lens - xi_model)/R ) # SY
+
+        return ska, wka, gam1, gam2 # SY
+
+
+def get_radec(pos):
+    ra = N.arctan(pos[1]/pos[0]) + N.pi + N.pi*(pos[0]>0)
+    ra -= 2*N.pi*(ra>2*N.pi)
+    dec = N.arcsin(pos[2]/N.sqrt(pos[0]**2+pos[1]**2+pos[2]**2))
+    return ra, dec
+
+
+
+def compute_kappa(p):
+    tmp = kappa.get_kappa(p)
+    return tmp
+            
+
+if __name__=='__main__':
+
+    parser = configargparse.ArgParser()
+
+    parser.add('--deltas', required=True, type=str, \
+               help='folder containing deltas in pix format')
+    parser.add('--model', required=True, \
+               help='text file containing model')
+    parser.add('--out', required=True, \
+               help='output fits file with kappa map')
+    parser.add('--gout', required=True, \
+               help='output fits file with gamma maps') # SY
+    parser.add('--nproc', required=False, type=int, default=1, \
+               help='number of procs used in calculation')
+    parser.add('--nspec', required=False, type=int, default=None, \
+               help='number of spectra to process')
+    parser.add('--rt_min', required=False, type=float, default=3., \
+               help='minimum transverse separation')
+    parser.add('--rp_min', required=False, type=float, default=3., \
+               help='minimum radial separation')
+    parser.add('--rt_max', required=False, type=float, default=40., \
+               help='maximum transverse separation')
+    parser.add('--rp_max', required=False, type=float, default=10., \
+               help='maximum radial separation')
+    parser.add('--true_corr', required=False, default=False,\
+               action='store_true', help='use actual lensed correlation')
+    args, unknown = parser.parse_known_args()
+
+    kappa.true_corr = args.true_corr
+    kappa.rt_min = args.rt_min
+    kappa.rp_min = args.rp_min
+    kappa.rt_max = args.rt_max
+    kappa.rp_max = args.rp_max
+    kappa.load_model(args.model)
+    kappa.read_deltas(args.deltas, nspec=args.nspec)
+    kappa.fill_neighs()
+
+    cpu_data = {}
+    for p in kappa.data.keys():
+        cpu_data[p] = [p]
+
+    print(' ', len(kappa.data.keys()), 'pixels with data')
+    pool = Pool(processes=args.nproc)
+    results = pool.map(compute_kappa, cpu_data.values())
+    pool.close()
+    print('')
+
+    #-- compiling results from pool
+    kap = sp.zeros(12*kappa.nside**2)
+    wkap = sp.zeros(12*kappa.nside**2)
+    ga1 = sp.zeros(12*kappa.nside**2) # SY
+    ga2 = sp.zeros(12*kappa.nside**2) # SY
+    wgam = sp.zeros(12*kappa.nside**2) # SY
+    for i, r in enumerate(results):
+        print(i, len(results))
+        index = N.array(r[0])
+        for j in index:
+            kap[j]  += r[1][j]
+            wkap[j] += r[2][j]
+            ga1[j]  += r[3][j]    # SY
+            ga2[j]  += r[4][j]    # SY
+            wgam[j] += r[2][j] *2 # SY
+    
+    w = wkap>0
+    kap[w]/= wkap[w]
+    ga1[w]/= wgam[w] # SY
+    ga2[w]/= wgam[w] # SY
+    
+    out = fitsio.FITS(args.out, 'rw', clobber=True)
+    head = {}
+    head['RPMIN']=kappa.rp_min
+    head['RPMAX']=kappa.rp_max
+    head['RTMIN']=kappa.rt_min
+    head['RTMAX']=kappa.rt_max
+    head['NT']=kappa.nt
+    head['NP']=kappa.np
+    head['NSIDE']=kappa.nside
+    out.write([kap, wkap], names=['kappa', 'wkappa'], header=head)
+    out.close()
+
+    # SY
+    out = fitsio.FITS(args.gout, 'rw', clobber=True)
+    head = {}
+    head['RPMIN']=kappa.rp_min
+    head['RPMAX']=kappa.rp_max
+    head['RTMIN']=kappa.rt_min
+    head['RTMAX']=kappa.rt_max
+    head['NT']=kappa.nt
+    head['NP']=kappa.np
+    head['NSIDE']=kappa.nside
+    out.write([ga1, ga2, wgam], names=['gamma1', 'gamma2', 'wgamma'], header=head)
+    out.close()
+
+
+
---
+++ bin/plot_gammas.py
@@ -0,0 +1,214 @@
+## SY
+
+from astropy.io import fits
+import healpy as hp
+import numpy as np
+import pylab as P
+import kappa_lya
+from kappa_lya import *
+import sys
+
+
+def open_gaussian():
+
+    g1 = fits.open('gamma-gaussian-true-v3.fits.gz')[1].data.gamma1
+    g2 = fits.open('gamma-gaussian-true-v3.fits.gz')[1].data.gamma2
+    wgam = fits.open('gamma-gaussian-true-v3.fits.gz')[1].data.wgamma
+    kap = fits.open('kappa-gaussian-true-v3.fits.gz')[1].data.kappa
+    wkap = fits.open('kappa-gaussian-true-v3.fits.gz')[1].data.wkappa
+    ktrue = fits.open('kappa_input.fits')[1].data.I
+    return g1, g2, wgam, kap, wkap, ktrue
+
+
+def open_blob():
+
+    g1 = fits.open('gamma-blob.fits.gz')[1].data.gamma1
+    g2 = fits.open('gamma-blob.fits.gz')[1].data.gamma2
+    wgam = fits.open('gamma-blob.fits.gz')[1].data.wgamma
+    kap = fits.open('kappa-blob.fits.gz')[1].data.kappa
+    wkap = fits.open('kappa-blob.fits.gz')[1].data.wkappa
+    ktrue = fits.open('kappa_blob_input.fits')[1].data.I
+    return g1, g2, wgam, kap, wkap, ktrue
+
+
+def plot_mollviews(ga1, ga2, k_g1, k_g2, kt, kg):
+
+    hp.mollview(ga1, min=-0.5, max=0.5, title = 'gamma1 (blob) estimator')
+    P.savefig('plots/gamma1_estim_blob.png')
+    P.close()
+
+    hp.mollview(k_g1, min=-0.5, max=0.5, title = 'gamma1 (blob) input')
+    P.savefig('plots/gamma1_input_blob.png')
+    P.close()
+
+    hp.mollview(ga2, min=-0.5, max=0.5, title = 'gamma2 (blob) estimator')
+    P.savefig('plots/gamma2_estim_blob.png')
+    P.close()
+
+    hp.mollview(k_g2, min=-0.5, max=0.5, title = 'gamma2 (blob) input')
+    P.savefig('plots/gamma2_input_blob.png')
+    P.close()
+
+    hp.mollview(kt, min=-0.5, max=0.5, title = 'kappa (blob) input')
+    P.savefig('plots/kappa_input_blob.png')
+    P.close()
+
+    hp.mollview(kg, min=-0.5, max=0.5, title = 'kappa (blob) estimator')
+    P.savefig('plots/kappa_estim_blob.png')
+    P.close()
+
+
+def plot_gamma(ga1, ga2, k_g1, k_g2):
+
+    # Get the theoretical values for ells and C_ells
+    th = kappa_lya.Theory()
+    ell, cell = th.get_cl_kappa(2.1)
+
+    # Adjust kappa C_ells to get gamma C_ells
+    # from equations 48 - 51 in Castro et al
+    for i, c_ell in enumerate(cell):
+        l = i + 1
+        cell[i] = (c_ell*(l+2)*(l-1))/(l*(l+1))
+
+    # Get the angular power spectrum (Cls) for auto- and cross-correlations
+    Cls1=hp.sphtfunc.anafast(ga1, lmax=3*NSIDE-1)
+    Cls2=hp.sphtfunc.anafast(ga2, lmax=3*NSIDE-1)
+    Cls3=hp.sphtfunc.anafast(k_g1, lmax=3*NSIDE-1)
+    Cls4=hp.sphtfunc.anafast(k_g2, lmax=3*NSIDE-1)
+    Cls12=hp.sphtfunc.anafast(ga1, ga2, lmax=3*NSIDE-1)
+    Cls13=hp.sphtfunc.anafast(ga1, k_g1, lmax=3*NSIDE-1)
+    Cls24=hp.sphtfunc.anafast(ga2, k_g2, lmax=3*NSIDE-1)
+
+    # gamma1 plot (divide cell by 4 to match eBOSS footprint
+    #              and by 2 because gamma?)
+    P.plot(ell, cell/8, label='Theory')
+    P.plot(Cls1, label='$\gamma_{1 est}$')
+    P.plot(Cls3, label='$\gamma_{1 true}$')
+    P.plot(Cls13, label='$\gamma_{1 est}\gamma_{1 true}$')
+    P.plot(Cls12, label='$\gamma_{1 est}\gamma_{2 est}$')
+    P.xlim(10,200)
+    P.ylabel('$C_l$', fontsize=18)
+    P.xlabel('$l$', fontsize=18)
+    P.legend()
+    P.title(r'$\gamma {\rm \ 1 \ Correlations}$')
+    P.savefig('plots/gamma1_corr.png')
+    P.close()
+
+    # gamma2 plot
+    P.plot(ell, cell/8, label='Theory')
+    P.plot(Cls2, label='$\gamma_{2 est}$')
+    P.plot(Cls4, label='$\gamma_{2 true}$')
+    P.plot(Cls24, label='$\gamma_{2 est}\gamma_{2 true}$')
+    P.xlim(10,200)
+    P.ylabel('$C_l$', fontsize=18)
+    P.xlabel('$l$', fontsize=18)
+    P.legend()
+    P.title(r'$\gamma {\rm \ 2 \ Correlations}$')
+    P.savefig('plots/gamma2_corr.png')
+    P.close()
+
+
+def plot_kappa(ga1, ga2, ke):
+
+    # make a 3D array using the shear components
+    a3darraymap = np.array([ga1*0.0, ga1, ga2]) 
+
+    # find nside of map from just input map
+    nside = hp.npix2nside(len(a3darraymap[1])) 
+    lmax=2*nside-1
+    nell = hp.sphtfunc.Alm.getsize(lmax=lmax)
+    
+    # put ell modes in an array
+    lmode = hp.sphtfunc.Alm.getlm(lmax=lmax, i=np.arange(nell))[0] 
+
+    # get alm from input map
+    alms = hp.sphtfunc.map2alm(a3darraymap, lmax=lmax, pol=True)
+
+    # ell mode coefficients to go from E_alm to kappa
+    lfactor_kappa = lmode*(lmode+1)/(lmode+2)/np.sqrt(lmode-1) 
+    lfactor_kappa[lmode<2]=0
+
+    # Get C_ells auto- for kappa estimated from gammas
+    Cls1 = lfactor_kappa*alms[1]
+
+    # Get the C_ells auto- for kappa estimated directly
+    Cls2=hp.sphtfunc.anafast(ke, lmax=3*NSIDE-1)
+
+    # Get the theoretical values for ells and C_ells
+    th = kappa_lya.Theory()
+    ell, cell = th.get_cl_kappa(2.1)
+
+    # kappa plot
+    P.figure(figsize=(10,8))
+    P.plot(ell, cell/4, label='Theory')
+    P.plot(Cls1, label='$\kappa_{\gamma_{1}\gamma_{2}} $')
+    P.plot(Cls2, label='$\kappa_{est} $')
+    P.xlim(10,200)
+    P.ylabel('$C_l$', fontsize=18)
+    P.xlabel('$l$', fontsize=18)
+    P.legend()
+    P.title(r'$\kappa {\rm \ Correlations}$')
+    P.savefig('plots/kappa_corr.png')
+    P.close()
+
+
+
+# input: enter 'g' or 'b' for gaussian / blob
+filetype = sys.argv[1]
+# input: enter 'g' or 'k' for gamma / kappa or 'n' for none
+outtype = sys.argv[2]
+
+# Open files
+if (filetype == 'b'):
+    g1, g2, wgam, kap, wkap, ktrue = open_blob()
+else:
+    g1, g2, wgam, kap, wkap, ktrue = open_gaussian()
+
+# Reset nside of input kappa file to match lower resolution maps
+NSIDE=int(hp.npix2nside(kap.size))
+alm = hp.sphtfunc.map2alm(ktrue, lmax=3*NSIDE-1)
+ktrue = hp.sphtfunc.alm2map(alm, nside=NSIDE)
+
+# Mask off areas outside eBOSS footprint
+w = (wkap == 0)
+kap[w] = hp.UNSEEN
+ktrue[w] = hp.UNSEEN
+g1[w] = hp.UNSEEN
+g2[w] = hp.UNSEEN
+
+# Get gamma1 and gamma2 from input kappa file
+kappa_alm = hp.map2alm(ktrue, lmax=3*NSIDE-1)
+
+# Get coefficients in ell and em
+LMAX=3*NSIDE-1
+lmode , em =hp.sphtfunc.Alm.getlm(lmax=LMAX)
+
+#inverse of coefficients that relate the kappa field and the "E mode type" measurement from lensing shear.
+LFACSEB=(lmode*(lmode+1)/((lmode+2)*(lmode-1)))**(-0.5)  
+
+LFACSEB[np.isinf(LFACSEB)]=0
+LFACSEB[np.isnan(LFACSEB)]=0
+
+# Get an E mode in spherical space from this kappa
+Etruealm=LFACSEB*kappa_alm 
+
+# Get gamma1 and gamma2 from E modes
+[kk, kapg1, kapg2] = hp.alm2map([Etruealm*0.0, Etruealm, Etruealm*0.0], \
+                       nside=NSIDE, pol=True)
+
+# Mask off areas outside eBOSS footprint
+kapg1[w] = hp.UNSEEN
+kapg2[w] = hp.UNSEEN
+
+# Make plots
+if (outtype == 'g'):
+    plot_gamma(g1, g2, kapg1, kapg2)
+elif (outtype == 'k'):
+    #plot_kappa(kapg1, kapg2, kap)
+    plot_kappa(g1, g2, kap)
+else:
+    plot_mollviews(g1, g2, kapg1, kapg2, ktrue, kap)
+
+
+
+
