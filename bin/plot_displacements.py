c13883f37861c2c99d8b3c08dad3cc8210dd84ba
Samantha Youles <youless@node246.prv.sciama.cluster>
2018-07-03 12:02:05 +0100

    Updated plot_displacements for new structure of lensed_deltas

--- bin/plot_displacements.py
+++ bin/plot_displacements.py
@@ -4,30 +4,29 @@
 import numpy as N
 import pylab as P
 from astropy.table import Table
+import sys
+from collections import OrderedDict ## For removing duplicate labels in plot
+
+# input directory name (e.g. outputs/table-blob.txt)
+fin1 = sys.argv[1]
+
+#P.rcParams.update({'font.size':22})
 
 ## Create a table of unlensed and lensed mocks qsos
-t = Table(N.loadtxt('outputs/table-blob.txt', skiprows=1), names=('ra', 'dec', 'ra_lens', 'dec_lens', 'z', 'plate', 'mjd', 'fiberid', 'thing_id'))
+t = Table(N.loadtxt(fin1, skiprows=1), names=('ra', 'dec', 'ra_lens', 'dec_lens', 'z', 'plate', 'mjd', 'fiberid', 'thing_id'))
 
-P.figure()
-
-P.plot(t['ra'], t['dec'], '.', color='k')
-P.plot(t['ra_lens'], t['dec_lens'], 'r', color='r')  ## 
-P.savefig('plots/all_qsos.png')
-
-## Pick some random numbers to use as indices for plotting a thinned out sample of the data
+## Pick random numbers to use as indices for plotting a thinned out sample of the data
 rand_ind = N.random.choice(N.arange(len(t)), replace=False, size=1000)
 
 P.figure(figsize=(8, 8))
 for i in rand_ind:
-    P.plot([t['ra'][i], t['ra_lens'][i]], [t['dec'][i], t['dec_lens'][i]], color='k')
-    P.plot(t['ra_lens'][i], t['dec_lens'][i], '.', color='r')
+    x = t['ra'][i]
+    y = t['dec'][i]
+    dx = t['ra_lens'][i] - t['ra'][i]
+    dy = t['dec_lens'][i] - t['dec'][i]
+    P.arrow(x, y, dx, dy, color='k', width=0.0003)
 P.xlim((2.6,3.6))
 P.ylim((0.0,1.0))
-#P.xlim((2,4))
-#P.ylim((0.2,0.8))
 P.title('Displacements in Mock QSOs')
 P.savefig('plots/sample_qsos.png')
-print(sum(t['ra_lens']-t['ra']!=0))
-print(sum(t['ra_lens']-t['ra']==0))
-
 
