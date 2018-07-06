b9659044d49dc8ac1f5511d340f9703aa24ddcbc
Samantha Youles <youless@node246.prv.sciama.cluster>
2018-07-06 15:56:10 +0100

    Reduced power in theoretical C_ells to match eBOSS footprint

--- bin/plot_gammas.py
+++ bin/plot_gammas.py
@@ -30,13 +30,13 @@ def open_blob():
     return g1, g2, kap, wkap, ktrue
 
 
-def plot_mollviews(ga1, ga2, k_g1, k_g2, kt, kg):
+def plot_mollviews(ga1, ga2, g1true, g2true, kt, kg):
 
     hp.mollview(ga1, min=-0.5, max=0.5, title = 'gamma1 (blob) estimator')
     P.savefig('plots/gamma1_estim_blob.png')
     P.close()
 
-    hp.mollview(k_g1, min=-0.5, max=0.5, title = 'gamma1 (blob) input')
+    hp.mollview(g1true, min=-0.5, max=0.5, title = 'gamma1 (blob) input')
     P.savefig('plots/gamma1_input_blob.png')
     P.close()
 
@@ -44,7 +44,7 @@ def plot_mollviews(ga1, ga2, k_g1, k_g2, kt, kg):
     P.savefig('plots/gamma2_estim_blob.png')
     P.close()
 
-    hp.mollview(k_g2, min=-0.5, max=0.5, title = 'gamma2 (blob) input')
+    hp.mollview(g2true, min=-0.5, max=0.5, title = 'gamma2 (blob) input')
     P.savefig('plots/gamma2_input_blob.png')
     P.close()
 
@@ -57,7 +57,7 @@ def plot_mollviews(ga1, ga2, k_g1, k_g2, kt, kg):
     P.close()
 
 
-def plot_gamma(ga1, ga2, k_g1, k_g2):
+def plot_gamma(ga1, ga2, g1true, g2true, mask_size):
 
     # Get the theoretical values for ells and C_ells
     th = kappa_lya.Theory()
@@ -72,41 +72,42 @@ def plot_gamma(ga1, ga2, k_g1, k_g2):
     # Get the angular power spectrum (Cls) for auto- and cross-correlations
     Cls1=hp.sphtfunc.anafast(ga1, lmax=3*NSIDE-1)
     Cls2=hp.sphtfunc.anafast(ga2, lmax=3*NSIDE-1)
-    Cls3=hp.sphtfunc.anafast(k_g1, lmax=3*NSIDE-1)
-    Cls4=hp.sphtfunc.anafast(k_g2, lmax=3*NSIDE-1)
+    Cls3=hp.sphtfunc.anafast(g1true, lmax=3*NSIDE-1)
+    Cls4=hp.sphtfunc.anafast(g2true, lmax=3*NSIDE-1)
     Cls12=hp.sphtfunc.anafast(ga1, ga2, lmax=3*NSIDE-1)
-    Cls13=hp.sphtfunc.anafast(ga1, k_g1, lmax=3*NSIDE-1)
-    Cls24=hp.sphtfunc.anafast(ga2, k_g2, lmax=3*NSIDE-1)
+    Cls13=hp.sphtfunc.anafast(ga1, g1true, lmax=3*NSIDE-1)
+    Cls24=hp.sphtfunc.anafast(ga2, g2true, lmax=3*NSIDE-1)
 
-    # gamma1 plot (divide cell by 4 to match eBOSS footprint
+    # gamma1 plot (Reduce cell by with mask to match eBOSS footprint
     #              and by 2 because gamma?)
-    P.plot(ell, cell/8, label='Theory')
-    P.plot(Cls1, label='$\gamma_{1 est}$')
-    P.plot(Cls3, label='$\gamma_{1 true}$')
-    P.plot(Cls13, label='$\gamma_{1 est}\gamma_{1 true}$')
+    P.plot(ell, (cell/2)*mask_size, label='Theory')
+    P.plot(Cls1, label='$\gamma_{1 est, est}$')
+    P.plot(Cls3, label='$\gamma_{1 true, true}$')
+    P.plot(Cls13, label='$\gamma_{1 est, true}$')
     P.xlim(10,200)
     P.ylabel('$C_l$', fontsize=18)
     P.xlabel('$l$', fontsize=18)
     P.legend()
     P.title(r'$\gamma {\rm \ 1 \ Correlations}$')
-    P.savefig('plots/gamma1_corr.png')
+    P.savefig('plots/gamma1_corr_c.png')
     P.close()
 
     # gamma2 plot
-    P.plot(ell, cell/8, label='Theory')
-    P.plot(Cls2, label='$\gamma_{2 est}$')
-    P.plot(Cls4, label='$\gamma_{2 true}$')
-    P.plot(Cls24, label='$\gamma_{2 est}\gamma_{2 true}$')
+    P.figure()
+    P.plot(ell, (cell/2)*mask_size, label='Theory')
+    P.plot(Cls2, label='$\gamma_{2 est, est}$')
+    P.plot(Cls4, label='$\gamma_{2 true, true}$')
+    P.plot(Cls24, label='$\gamma_{2 est, true}$')
     P.xlim(10,200)
     P.ylabel('$C_l$', fontsize=18)
     P.xlabel('$l$', fontsize=18)
     P.legend()
     P.title(r'$\gamma {\rm \ 2 \ Correlations}$')
-    P.savefig('plots/gamma2_corr.png')
+    P.savefig('plots/gamma2_corr_c.png')
     P.close()
 
 
-def plot_kappa(ga1, ga2, k_k, k_t):
+def plot_kappa(ga1, ga2, k_k, k_t, mask_size):
     '''Derive kappa_g from the estimated gamma1 and gamma2, and then plot
        auto- and cross-correlations'''
 
@@ -148,14 +149,16 @@ def plot_kappa(ga1, ga2, k_k, k_t):
     th = kappa_lya.Theory()
     ell, cell = th.get_cl_kappa(2.1)
 
-    # kappa plots
+    # kappa plot
+    P.ion()
     P.figure(figsize=(10,8))
-    # The factor of 4 below accounts for eBOSS footprint ~ 1/4 full sky
+    # The factor below accounts for eBOSS footprint ~ 0.184 of full sky
     #P.plot(ell, cell/4, label='Theory')
+    P.plot(ell, cell * mask_size, label='Theory')
 
-    #P.plot(Cls1, label='$\kappa_{\gamma \gamma} $')
-    #P.plot(Cls2, label='$\kappa_{\kappa \kappa} $')
-    #P.plot(Cls3, label='$\kappa_{tt} $')
+    P.plot(Cls1, label='$\kappa_{\gamma \gamma} $')
+    P.plot(Cls2, label='$\kappa_{\kappa \kappa} $')
+    P.plot(Cls3, label='$\kappa_{tt} $')
 
     #P.plot(Cls12, label='$\kappa_{\kappa \gamma} $')
     #P.plot(Cls13, label='$\kappa_{\gamma t} $')
@@ -164,9 +167,9 @@ def plot_kappa(ga1, ga2, k_k, k_t):
     #P.plot(Cls23/Cls3, label='$\kappa_{\kappa t} / \kappa_{tt} $')
     #P.plot(Cls13/Cls3, label='$\kappa_{\gamma t} / \kappa_{tt} $')
 
-    P.plot(Cls2 - Cls3, label='$\kappa_{\kappa \kappa} - \kappa_{tt} $')
-    P.plot(Cls1 - Cls3, label='$\kappa_{\gamma \gamma} - \kappa_{tt} $')
-    P.plot(Cls12 - Cls3, label='$\kappa_{\kappa \gamma} - \kappa_{tt} $')
+    #P.plot(Cls2 - Cls3, label='$\kappa_{\kappa \kappa} - \kappa_{tt} $')
+    #P.plot(Cls1 - Cls3, label='$\kappa_{\gamma \gamma} - \kappa_{tt} $')
+    #P.plot(Cls12 - Cls3, label='$\kappa_{\kappa \gamma} - \kappa_{tt} $')
 
     P.xlim(10,200)
     #P.ylim(-0.5e-8,2.1e-8)
@@ -174,23 +177,24 @@ def plot_kappa(ga1, ga2, k_k, k_t):
     P.xlabel('$l$', fontsize=18)
     P.legend(fontsize=22)
     P.title(r'$\kappa {\rm \ Correlations}$')
-    #P.savefig('plots/kappa_corr_b.png')
+    #P.savefig('plots/kappa_corr.png')
     #P.close()
 
 
 
 # input: enter 'g' or 'b' for gaussian / blob
-filetype = sys.argv[1]
+#filetype = sys.argv[1]
 # input: enter 'g' or 'k' for gamma / kappa or 'n' for none
-outtype = sys.argv[2]
+#outtype = sys.argv[2]
+
+filetype='g'
+outtype= 'k'
 
 # Open files
 if (filetype == 'b'):
     g1, g2, kap, wkap, ktrue = open_blob()
 else:
     g1, g2, kap, wkap, ktrue = open_gaussian()
-    #g1 = g1*2
-    #g2 = g2*2
 
 # Reset nside of input kappa file to match lower resolution maps
 NSIDE=int(hp.npix2nside(kap.size))
@@ -199,6 +203,7 @@ ktrue = hp.sphtfunc.alm2map(alm, nside=NSIDE)
 
 # Mask off areas outside eBOSS footprint
 w = (wkap == 0)
+mask_size = 1 - wkap[w].size/wkap.size
 kap[w] = hp.UNSEEN
 ktrue[w] = hp.UNSEEN
 g1[w] = hp.UNSEEN
@@ -231,9 +236,9 @@ g2t[w] = hp.UNSEEN
 
 # Make plots
 if (outtype == 'g'):
-    plot_gamma(g1, g2, g1t, g2t)
+    plot_gamma(g1, g2, g1t, g2t, mask_size)
 elif (outtype == 'k'):
-    plot_kappa(g1, g2, kap, ktrue)
+    plot_kappa(g1, g2, kap, ktrue, mask_size)
 else:
     plot_mollviews(g1, g2, g1t, g2t, ktrue, kap)
 
