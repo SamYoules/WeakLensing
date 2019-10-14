5f106b33d38f62fc49339b1c8d1b5544ec883ce3
Samantha Youles <youless@login1.pri.sciama3.alces.network>
2019-10-14 17:30:36 +0100

    Corrected plot_mc_with_errors.py

--- bin/plot_mc_with_errors.py
+++ bin/plot_mc_with_errors.py
@@ -7,24 +7,28 @@ import pylab as plt
 from astropy.table import Table
 import glob
 
-rt = 30
-rp = 10
+rt     = 30
+rp     = 10
 mcreal = 1000000
-nreal = 100
-var = 0.05
-kin = np.array([-0.14, -0.12, -0.1, -0.08, -0.06, -0.04, -0.02, 0.,
-             0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14])
-residual = np.zeros(kin.size)
+nreal  = 100
+var    = 0.05
+kin    = np.array([-0.14, -0.12, -0.1, -0.08, -0.06, -0.04, -0.02, 0.,
+                    0.02, 0.04, 0.06, 0.08, 0.1, 0.12, 0.14])
+
+res_sum   = np.zeros(kin.size)
+res_sumsq = np.zeros(kin.size)
+
 indir = 'monte_carlo_1'
 
 allfiles = glob.glob(indir+'/*.fits') 
 for i, filename in enumerate(allfiles):
     t = Table.read(filename)
-    residual+=(t['kappa_out']-kin)
+    res_sum   += t['kappa_out']-kin
+    res_sumsq += (t['kappa_out']-kin)**2
 
-#-- Get RMSE of residual
-rmse = np.sqrt(residual**2 / nreal)
-mean_resid = residual / nreal
+#-- Get mean and RMSE of residual
+mean_resid = res_sum / nreal
+rmse = np.sqrt(res_sumsq / nreal)
 
 #-- plot the mean residuals with the error bars
 plt.figure()
